83b2e1cf21a04de84d487ced4bdb2fd74dff0fc7
Adit Modi <adit.modi24@gmail.com>
2020-09-11 10:53:05 +0530

    Update athenaquery.sql

--- athenaquery.sql
+++ athenaquery.sql
@@ -6,3 +6,4 @@ CREATE EXTERNAL TABLE heartrate_iot_data (
 ROW FORMAT  serde 'org.apache.hive.hcatalog.data.JsonSerDe'
 with serdeproperties( 'ignore.malformed.json' = 'true' )
 LOCATION 's3://<CREATED-BUCKET>/iot_to_bi_example/'
+
